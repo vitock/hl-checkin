27706e3411223ed839b2b91222d837eb3a6bd3da
liw <liw>
2021-09-29 19:02:55 +0800

    update

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
-fileName="pack_$(date "+%m-%d_%H_%M").zip"
-rm ${fileName} 
+fileName="package_$(date "+%m-%d_%H_%M_%S").zip"
+rm package_* 
 zip   -r ${fileName}   icons app.js style.css manifest.json
  
